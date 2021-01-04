Fixed normalization

scripts/extract_STP_data.py
@@ -31,7 +31,9 @@ results = pandas.DataFrame(columns=['pair_id',
                                     'clamp_mode',
                                     'ind_freq',
                                     'rec_delay',
-                                    'amps']
+                                    'amps',
+                                    'psc_amp',
+                                    'psp_amp']
                            )
 
 for ix in range(len(pairs)):
@@ -58,6 +60,11 @@ for ix in range(len(pairs)):
                 if 1 not in pulses or 2 not in pulses:
                     continue
                 amps = {k: r.PulseResponseFit.fit_amp for k, r in pulses.items()}
+                if clamp_mode == 'vc':
+                    normamps = np.array(list(amps.values())) / pair.synapse.psc_amplitude
+
+                elif clamp_mode == 'ic':
+                    normamps = np.array(list(amps.values())) / pair.synapse.psp_amplitude
 
                 results = results.append({'pair_id': pair.id,
                                           'pre_cell': pair.pre_cell.cell_class,
@@ -66,29 +73,13 @@ for ix in range(len(pairs)):
                                           'clamp_mode': clamp_mode,
                                           'ind_freq': ind_freq,
                                           'rec_delay': rec_delay,
-                                          'amps': list(amps.values())},
+                                          'amps': np.array(list(amps.values())),
+                                          'normamps': normamps,
+                                          'stimuli': list(amps.keys()),
+                                          'psc_amp': pair.synapse.psc_amplitude,
+                                          'psp_amp': pair.synapse.psp_amplitude},
                                          ignore_index=True)
 
-def calc_norm_amplitudes(results):
-
-    initialAmp = {}
-    # iterate over pairs
-    for id in list(results.pair_id.unique()):
-        subdat = results[results.pair_id==id]
-        vcdat = subdat[subdat.clamp_mode == 'vc']
-        icdat = subdat[subdat.clamp_mode == 'ic']
-        initialAmp[id] = {'vc': np.array([vcdat.amps.iloc[i][0] for i in range(len(vcdat))]).mean(),
-                          'ic': np.array([icdat.amps.iloc[i][0] for i in range(len(icdat))]).mean()}
-
-    normAmp = []
-
-    for i in range(len(results)):
-        s = results.iloc[i]
-        normAmp.append(np.array(s.amps) / initialAmp[s.pair_id][s.clamp_mode])
-
-    results['normAmps'] = normAmp
-
-    return results
 
 def get_amps_first8(results, normalized=False):
     """
@@ -99,24 +90,23 @@ def get_amps_first8(results, normalized=False):
 
     for ix in range(len(results)):
         if normalized:
-            amps = results.iloc[ix].normAmps
+            amps = results.iloc[ix].normamps
         else:
             amps = results.iloc[ix].amps
 
-            if len(amps) >= 8:
-                x[ix, :] = amps[:8]
-            else:
-                x[ix, :] = np.pad(amps, (0, 8-len(amps)), constant_values=np.nan)
+        if len(amps) >= 8:
+            x[ix, :] = amps[:8]
+        else:
+            x[ix, :] = np.pad(amps, (0, 8-len(amps)), constant_values=np.nan)
 
     return x
 
 
 # Example
-x = get_amps_first8(results[(results.ind_freq==100.0) & (results.clamp_mode == 'vc')])
+x = get_amps_first8(results[(results.ind_freq==100.0) & (results.clamp_mode == 'vc')], normalized=True)
 
 plt.plot(-x.T*(10**12), lw=0.2, color='gray')
 plt.plot(np.nanmean(-x*(10**12), 0), lw=2, color='green')
-plt.ylim(0, 75)
 plt.xlabel('# stim')
 plt.ylabel('EPSC amplitude (pA)')
 plt.title('E-E 100Hz')
@@ -124,11 +114,10 @@ plt.title('E-E 100Hz')
 plt.savefig('example_EE_100Hz')
 plt.show()
 
-x = get_amps_first8(results[(results.ind_freq==50.0) & (results.clamp_mode == 'vc')])
+x = get_amps_first8(results[(results.ind_freq==50.0) & (results.clamp_mode == 'vc')], normalized=True)
 
 plt.plot(-x.T*(10**12), lw=0.2, color='gray')
 plt.plot(np.nanmean(-x*(10**12), 0), lw=2, color='green')
-plt.ylim(0, 75)
 plt.xlabel('# stim')
 plt.ylabel('EPSC amplitude (pA)')
 plt.title('E-E 50Hz')